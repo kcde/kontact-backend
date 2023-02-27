54e3bb3035e43a3e238dfd6715cc7394ad52527f
keside <wilsonezeala@yahoo.com>
2023-02-27 12:46:13 +0100

    chore: set up mongo db connection to cluster

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,32 @@
-console.log('server file');
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose, { ConnectOptions } from 'mongoose';
+
+const app: express.Application = express();
+dotenv.config();
+const PORT = 3456;
+
+const { MONGO_URL } = process.env;
+
+app.listen(PORT, async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    //connect mongoose
+    await mongoose.connect(
+      MONGO_URL as unknown as string,
+      {
+        useNewUrlParser: true
+      } as ConnectOptions
+    );
+
+    console.log('mongo connected');
+  } catch (error) {
+    console.log(error);
+
+    console.log('unable to ocnnect to mongo');
+  }
+
+  console.log(`App running on port   ${PORT}`);
+});
+
+dotenv.config();
